6ed1ae4b38b263fa87407855802e7deb0ce1ebbd
Cyenoch <cyenoch@outlook.com>
2024-02-05 19:05:31 +0800

    ..

--- network/generate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cryptogen generate --config=./crypto-config.yaml
-configtxgen -outputBlock system_genesis_block/genesis_block.pb -profile ChannelUsingRaft --channelID application-channel-1 -configPath ./
