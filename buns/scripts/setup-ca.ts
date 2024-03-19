796dafb9eb59995672a605ab65fb5b66d85c4280
Cyenoch <cyenoch@outlook.com>
2024-03-19 20:59:24 +0800

    update

--- buns/scripts/setup-ca.ts
+++ buns/scripts/setup-ca.ts
@@ -78,7 +78,7 @@ async function enrollOrgCA(
   await $`mkdir -p ${home}/ca`
   await $`cp ${caCert} ${home}/ca/ca.${host}-cert.pem`
 
-  console.info(`注册 users ${host}`)
+  console.info(`注册用户user0、orderer0、admin0 ${host}`)
   if (kind === 'peer') {
     await $`fabric-ca-client register --caname ${caName} --id.name peer0 --id.secret peer0pw --id.type peer --tls.certfiles ${caCert}`
     await $`fabric-ca-client register --caname ${caName} --id.name user0 --id.affiliation ${userAffiliation} --id.secret user0pw --id.type client --tls.certfiles ${caCert}`
@@ -88,8 +88,10 @@ async function enrollOrgCA(
 
   await $`fabric-ca-client register --caname ${caName} --id.name admin0 --id.secret admin0pw --id.type admin --tls.certfiles ${caCert}`
 
+  console.info(`注册用户user0、orderer0、admin0 ${host} 注册成功`)
+
   if (kind === 'peer') {
-    console.info(`生成 peer0 MSP ${host}`)
+    console.info(`生成 peer0 MSP ${host} 获取证书`)
     await $`fabric-ca-client enroll -u https://peer0:peer0pw@${caHost}:${caPort} --caname ${caName} -M ${peer0Home}/msp --tls.certfiles ${caCert}`
     await $`cp ${home}/msp/config.yaml ${peer0Home}/msp/config.yaml`
     await $`fabric-ca-client enroll -u https://peer0:peer0pw@${caHost}:${caPort} --caname ${caName} -M ${peer0Home}/tls --enrollment.profile tls --csr.hosts peer0.${host} --csr.hosts ${caHost} --tls.certfiles ${caCert}`
@@ -97,16 +99,19 @@ async function enrollOrgCA(
     await $`cp ${peer0Home}/tls/tlscacerts/tls-${caHost}-${caPort}-${caName}.pem ${peer0Home}/tls/ca.crt`
     await $`cp ${peer0Home}/tls/signcerts/cert.pem ${peer0Home}/tls/server.crt`
     await $`cp ${await firstFile(`${peer0Home}/tls/keystore/`)} ${peer0Home}/tls/server.key`
+    console.info(`生成 peer0 MSP ${host} 获取证书 成功`)
 
 
-    console.info(`生成 user0 MSP ${host}`)
+    console.info(`生成 user0 MSP ${host} 获取证书`)
     await $`fabric-ca-client enroll -u https://user0:user0pw@${caHost}:${caPort} --caname ${caName} -M ${user0Home}/msp --tls.certfiles ${caCert}`
     await $`cp ${home}/msp/config.yaml ${user0Home}/msp/config.yaml`
+    console.info(`生成 user0 MSP ${host} 获取证书 成功`)
   } else {
-    console.info(`生成 orderer0 MSP ${host}`)
+    console.info(`生成 orderer0 MSP ${host} 获取证书`)
     await $`fabric-ca-client enroll -u https://orderer0:orderer0pw@${caHost}:${caPort} --caname ${caName} -M ${peer0Home}/msp --tls.certfiles ${caCert}`
     await $`cp ${home}/msp/config.yaml ${peer0Home}/msp/config.yaml`
     await $`fabric-ca-client enroll -u https://orderer0:orderer0pw@${caHost}:${caPort} --caname ${caName} -M ${peer0Home}/tls --enrollment.profile tls --csr.hosts orderer0.${host} --csr.hosts ${caHost} --tls.certfiles ${caCert}`
+    console.info(`生成 orderer0 MSP ${host} 获取证书 成功`)
 
     await $`cp ${peer0Home}/tls/tlscacerts/tls-${caHost}-${caPort}-${caName}.pem ${peer0Home}/tls/ca.crt`
     await $`cp ${peer0Home}/tls/signcerts/cert.pem ${peer0Home}/tls/server.crt`
